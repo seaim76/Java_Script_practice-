39aa14e9b59bd581e529911f93ee194ef1a5bec1
Seaim76 <seaim76@gmail.com>
2022-07-13 15:59:25 +0600

    2nd day

--- ex.js
+++ ex.js
@@ -1,2 +1,32 @@
 console.log("Seaim")
 console.log(47)
+for (var i = 1; i < 10; i++)
+    console.log(i)
+    //Print
+console.log('Bye Bye')
+
+//Variable
+
+var BananaPrice = 12;
+console.log("Banana Price " + BananaPrice)
+
+var age = 19;
+console.log("Age " + age)
+
+var year = 2022;
+console.log("Year " + year)
+
+//Type of
+var seenAfter = 21;
+console.log(typeof seenAfter)
+
+var name1 = "seaim"
+console.log(name1)
+
+var name = "seaim"
+console.log(typeof name)
+
+var isHot = true;
+console.log(typeof isHot);
+var isRich = false;
+console.log(isRich);
