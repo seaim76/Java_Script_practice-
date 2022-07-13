af55dbcc20bd4ac16e9870383cad13384fb43cd2
Seaim76 <seaim76@gmail.com>
2022-07-13 19:17:20 +0600

    Basic Done

--- ex.js
+++ ex.js
@@ -19,7 +19,7 @@ console.log("Year " + year)
 //Type of
 var seenAfter = 21;
 console.log(typeof seenAfter)
-
+    //declare many var
 var name1 = "seaim"
 console.log(name1)
 
@@ -30,3 +30,77 @@ var isHot = true;
 console.log(typeof isHot);
 var isRich = false;
 console.log(isRich);
+//variable case sensetive
+//camel CASE
+var myName = 'Quatationname';
+Name1 = 'seaim';
+Name2 = 'karim';
+Name3 = 'QuatationName';
+Name4 = 'rahim';
+
+console.log(Name4);
+
+var promise = 'I promise I will Work Hard To Become Programmer';
+//lower case
+console.log(promise.toLowerCase());
+//uppercase
+console.log(promise.toUpperCase());
+//finding of position
+console.log(promise.indexOf('I'));
+//split
+console.log(promise.split('I '));
+
+//int float
+var number1 = 10;
+var number2 = '10.5';
+//var number2 = parseFloat(10.5);
+number2 = parseFloat(number2);
+console.log(number1 + number2);
+
+///convert into string
+var num1 = 0.1;
+var num2 = 0.2;
+num2 = " " + num2;
+console.log(typeof num2);
+
+//math
+var price1 = 25;
+var price2 = 25;
+var sum = console.log("Total price sum " + (price1 + price2));
+var sum = console.log("Total price sub " + (price1 - price2));
+var sum = console.log("Total price mul " + (price1 * price2));
+var sum = console.log("Total price div " + (price1 / price2));
+
+
+//Operator
+var p1 = 25;
+var p2 = 35;
+p3 = 20;
+p3--;
+p2 = p2 + 1;
+p1++;
+console.log(p1)
+console.log(p2)
+console.log(p3)
+
+//math absulate
+var numa = -50;
+numb = 1.60;
+//negative bad deba absulate value dekhabe
+var absulatenum = Math.abs(numa);
+console.log(absulatenum);
+//float bad deya deba
+var absulatenum = Math.round(numb);
+//barai deba
+console.log(absulatenum);
+var numc = 2.0055;
+var result = Math.ceil(numc);
+console.log(result);
+//komai diba
+var numd = 2.99999;
+var result = Math.floor(numd);
+console.log(result);
+
+var r = Math.random() * 100;
+var result = Math.round(r);
+console.log(result);
