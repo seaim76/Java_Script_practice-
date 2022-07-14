244aac81690c55d625d41bd774617e99d3146bff
Seaim76 <seaim76@gmail.com>
2022-07-14 17:32:06 +0600

    New commit

---
+++ .vscode/fundamentalConcept.js
@@ -0,0 +1,117 @@
+            // var age = [15, 17, 18, 20]
+            //    //changing array index
+            // age[1] = 22;
+
+            // var seaimAge = age[3];
+            // index print
+            // console.log(age[2])
+            // console.log(seaimAge)
+
+            // console.log(age.indexOf(20));
+            // console.log(age.length)
+
+            // age.push(500, 100);
+
+            // console.log(age.length)
+            // console.log(age);
+            // age.pop();
+            // console.log(age);
+            // age.unshift(10, 11, 12)
+            // console.log(age);
+            // age.shift(10, 11)
+            // console.log(age);
+
+
+
+            //4
+
+            // var teaLine = ['kalam', 'salam', 'balam']
+            // //add element last
+            // teaLine.push('talam', 'jalam')
+            // console.log(teaLine)
+            //     //remove element from last
+            // teaLine.pop()
+            // console.log(teaLine)
+            //     //remove element from first
+            // teaLine.shift();
+            // console.log(teaLine)
+            // //add element first
+            // teaLine.unshift('seaim');
+            // console.log(teaLine)
+            //slicing
+            // var teaLine = ['seaim', 'kalam', 'salam', 'balam', 'talam', 'palam', 'kuddos']
+            // console.log(teaLine);
+            // //star to end 
+            // var part = teaLine.slice(2, 6);
+            // console.log(part);
+
+
+            //While loop
+            // num = 10;
+            // while (num < 15) {
+            //     console.log(num);
+            //     num++;
+            // }
+
+            //for loop
+
+            // for (var i = 0; i < 10; i++) {
+            //     console.log(i);
+
+            // }
+
+            // var num = [10, 20, 30, 40, 50, 60]
+
+            // for (var i = 0; i < num.length; i ++) {
+            //     var element = num[i];
+            //     console.log(element)
+            // }
+
+            //Switch
+            // num = 1000;
+            // switch (num) {
+            //     case 1000:
+            //         if (num) {
+            //             console.log('i am 5 and if condition')
+
+
+
+            //         } else {
+            //             console.log('else')
+            //         }
+            //         break;
+
+            //     case 10:
+            //         console.log('i am 10')
+
+            //         break;
+
+            //     case 5:
+            //         console.log('i am 5')
+
+            //         break;
+
+            //     default:
+
+            //         console.log('i dont know')
+
+
+            //         break;
+            // }
+
+            //Multi case
+
+            // var animal = 'Dog'
+            // switch (animal) {
+            //     case 'cow':
+            //     case 'crow':
+            //     case 'Dog':
+            //         console.log('Noha\'s Ark')
+            //         break;
+            //     case 'Dinasor':
+
+            //     default:
+            //         break;
+            // }
+
+            //function
