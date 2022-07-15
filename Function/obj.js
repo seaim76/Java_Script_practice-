fd07cfa7c105db24038f0ad58fc6af2cdf61a768
Seaim76 <seaim76@gmail.com>
2022-07-15 15:29:09 +0600

    Fundamental js

--- Function/obj.js
+++ Function/obj.js
@@ -1,4 +1,21 @@
- var student = { id: 10, phone: 01532014144, name: "abir" };
- var student2 = { id: 11, phone: 016666144, name: "karim" };
+ var student = { id: 10, phone: 01628790526, name: "abir" };
+ var student2 = { id: 11, phone: 01710079354, name: "karim" };
+
+ var phoneNum = student.phone;
+
+ //Using array
+ var phonenumberA = student2["phone"]
+ console.log("Array " + phonenumberA);
+
+ var id1 = student.id;
+ console.log(id1);
+ console.log(phoneNum);
  console.log(student);
  console.log(student2);
+ //update
+ student.phone = 0246256454;
+ console.log(student.phone);
+ //add new property
+
+ student2.cenema = "Ogni";
+ console.log(student2.cenema);
