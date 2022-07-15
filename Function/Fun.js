a17630970a23c89a1bac79590fe122371b8d05d2
Seaim76 <seaim76@gmail.com>
2022-07-15 15:15:26 +0600

    JS P

---
+++ Function/Fun.js
@@ -0,0 +1,42 @@
+// function sayLoveYou() {
+//     console.log("Your message");
+//     console.log("I love Your messages");
+// }
+// sayLoveYou();
+
+// var date = 14;
+// var place = "Restaurent";
+// sayLoveYou();
+// var hangout = "tsc";
+// sayLoveYou();  
+//magic function
+
+// function doubleIt(num) {
+//     var result = num * 2;
+//     console.log("Result After doubleIt: " + result);
+
+// }
+// doubleIt(5);
+// doubleIt(100);
+// doubleIt(200);
+
+// function Number(num) {
+//     var result = num * 2;
+//     return result;
+
+// }
+
+// var first = Number(5);
+// var second = Number(10);
+// var total = (first + second)
+// console.log(total);
+
+
+
+function add(num1, num2) {
+    var result = num1 + num2;
+    return result;
+}
+
+var sum = add(15, 20);
+console.log(sum)
---
+++ Function/obj.js
@@ -0,0 +1,4 @@
+ var student = { id: 10, phone: 01532014144, name: "abir" };
+ var student2 = { id: 11, phone: 016666144, name: "karim" };
+ console.log(student);
+ console.log(student2);
