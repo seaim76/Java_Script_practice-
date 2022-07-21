60ac28ef3db43301f8cb4f66b95a93a7c5e4c0da
Seaim76 <seaim76@gmail.com>
2022-07-21 21:17:38 +0600

    New up

--- Use Case of Js/first.js
+++ Use Case of Js/first.js
@@ -1,1 +1,23 @@
-console.log("Seaim")
+// var inch = 156;
+// // Convert
+// // var feet = inch / 12;
+// // console.log(feet);
+
+
+// function inchToFeet(inch) {
+//     var feet = inch / 12;
+//     return feet;
+// }
+
+// var nana = inchToFeet(156);
+// console.log(nana);
+// var nani = inchToFeet(288);
+// console.log(nani);
+// var ami = inchToFeet(300);
+// console.log(ami);
+
+const name = "Javed Akber";
+if (name.length > 4) {
+    name = "JUJU"
+}
+console.log(name);
