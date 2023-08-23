c98dec794b71e26c751c78db3566d1e97e40e3af
Seaim <58206436+seaim76@users.noreply.github.com>
2023-08-23 20:43:38 +0000

    Operators

---
+++ operators.js
@@ -0,0 +1,8 @@
+var price1 =100;
+var price2 =11;
+
+console.log(" Price is " + price1+price2);
+console.log(" Price is " + price2-price1);
+console.log(" Price is " + price1*price2);
+console.log(" Price is " + price1/price2);
+console.log(" Price is " + price1%price2);
