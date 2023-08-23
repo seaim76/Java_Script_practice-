cab2f79bb2167139d634dc22165a428619decb2e
Seaim <58206436+seaim76@users.noreply.github.com>
2023-08-23 20:45:10 +0000

    String

---
+++ parse.js
@@ -0,0 +1,5 @@
+var a =10;
+var b =11;
+
+// b = parseInt(b);
+console.log(a + b);
---
+++ play_with_String.js
@@ -0,0 +1,4 @@
+var promise ="I will work very hard and I will become Programmer";
+console.log(promise.toUpperCase());
+console.log(promise.indexOf('work'));
+console.log(promise.split('I'));
