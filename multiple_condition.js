7236c6d70118feeee0cbf1f5586bb3bb344b47b0
Seaim <58206436+seaim76@users.noreply.github.com>
2023-08-23 21:11:52 +0000

    variable

---
+++ multiple_condition.js
@@ -0,0 +1,16 @@
+// &&
+// ||
+// !
+var job =true;
+var saving =8000;
+
+if(job && saving > 10000){
+console.log("Work Very hard for your Dream");
+}
+else if (job == true ){
+console.log("Work Hard");
+}
+
+else{
+console.log("'Go to hell'");
+}
---
+++ variable.js
@@ -0,0 +1,11 @@
+var banana_price = 10;
+var apple_price = 40;
+var ur_name = "Seaim";
+is_true =true;
+is_false =false;
+
+console.log("Banana Price is "+ banana_price);
+console.log("Banana Price is "+apple_price);
+console.log(typeof apple_price);
+console.log(typeof ur_name);
+console.log(typeof is_true);
