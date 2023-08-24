2153794c75ace9e0c9ba9a164c6027d2f00afa09
Seaim <58206436+seaim76@users.noreply.github.com>
2023-08-24 14:51:29 +0000

    array

---
+++ Fundamental/array.js
@@ -0,0 +1,14 @@
+// Array
+var arr =['seaim','rahim',10,50,60];
+console.log(arr);
+var position = arr.indexOf(50);
+console.log(position);
+var position1 = arr.indexOf(500);
+console.log(position1);
+// Before push
+console.log(arr);
+arr.push(100);
+// After push
+console.log(arr);
+arr.length;
+console.log(arr);
